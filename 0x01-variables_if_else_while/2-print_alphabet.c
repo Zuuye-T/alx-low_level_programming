ff65dd16850ae08ee9e9bcaea9be03e328237c1b
Ogunnowo Oluwatobi <davidogunnowo3@gmail.com>
2023-02-17 13:45:55 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	/* your code goes there */
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
