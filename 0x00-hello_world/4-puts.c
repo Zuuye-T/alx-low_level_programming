f97fbf168b233ba19e50d75efbabaf9583fd739c
Ogunnowo Oluwatobi <davidogunnowo3@gmail.com>
2023-02-18 02:52:47 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ *Return: always 0
+ *//**
+ * main - main function
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
