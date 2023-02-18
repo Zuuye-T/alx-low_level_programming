dd29c39f72e569683f3d9ee3126ba1be39dd8512
Ogunnowo Oluwatobi <davidogunnowo3@gmail.com>
2023-02-17 16:34:24 -0800

    betty coding style failure correction

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,10 +13,10 @@ int main(void)
 
 	for (i = 'a' ; i <= 'z'; i++)
 	{
-	       if (i != 'q' && i != 'e')
-	       {
-			putchar(i);
-		}
+	if (i != 'q' && i != 'e')
+	{
+	putchar(i);
+	}
 	}
 	putchar('\n');
 	return (0);
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+       
+	
+	return (0);
+}
