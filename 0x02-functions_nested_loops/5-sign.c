874404ff1f367f09e72723dfe564b1eb7551f303
Ogunnowo Oluwatobi <davidogunnowo3@gmail.com>
2023-02-21 14:13:08 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_sign -Entry point
+ *
+ * @n: is the number sign to be checked
+ *
+ * Return: 1 (Success) otherwise 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar ('-');
+		return (1);
+	}
+	else
+	{
+		_putchar ('0');
+		return (0);
+	}
+
+}
