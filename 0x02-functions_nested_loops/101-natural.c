2b0b08d0f1a70755108bf4266497069cc09bf7d7
Ogunnowo Oluwatobi <davidogunnowo3@gmail.com>
2023-02-21 17:00:23 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+	for (i = 3 ; i < 10 ; i++)
+	{
+		if ((1 % 3 == 0) || (1 % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d \n", sum);
+	return (0);
+}
