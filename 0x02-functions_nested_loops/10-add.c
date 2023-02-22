8910ae36ad09550a4167844680aac5cbcbb872dc
Ogunnowo Oluwatobi <davidogunnowo3@gmail.com>
2023-02-21 16:36:52 -0800

    9-times_table.c and 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * add - Entry point
+ *
+ * @i: is one of the integers
+ * @j: is the second
+ *
+ * Return: 1 (Success) otherwise 0
+ */
+
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,8 +22,25 @@ void times_table(void)
 		for (j = 0; j <= 9; j += 1)
 		{
 			int z = i * j;
-			_putchar (z + '0');
+
+			if (j == 0)
+			{
+				_putchar (z + '0');
+			}
+			else if (j != 0 && z <= 9)
+			{
 			_putchar (',');
+			_putchar (' ');
+			_putchar (' ');
+			_putchar (z + '0');
+			}
+			else if (z > 9)
+			{
+			_putchar (',');
+			_putchar (' ');
+			_putchar ((z / 10) + '0');
+			_putchar ((z % 10) + '0');
+			}
 		}
 		_putchar ('\n');
 	}
