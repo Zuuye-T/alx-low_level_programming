406154b2186bb9c7815f4db1fbb673d3d44b9fe4
Ogunnowo Oluwatobi <davidogunnowo3@gmail.com>
2023-02-23 16:26:04 -0800

    101-print_numbers.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - a function that prints an integer
+ * @n: input
+ * Return: inputted integer
  */
+
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
-		n1 = -n;
+		n *= -1;
+		k = n;
 		_putchar('-');
-	} else
-	{
-		n1 = n;
 	}
 
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
+	k /= 10;
 
-	_putchar((n1 % 10) + '0');
+	if (k != 0)
+		print_number(k);
 
-	return (0);
+	_putchar((unsigned int) n % 10 + '0');
+
 }
